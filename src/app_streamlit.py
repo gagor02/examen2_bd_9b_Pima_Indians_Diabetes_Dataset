223e75e509af20daf65ca28dd30b198fee6c3601
gagor02 <gagor0202@gmail.com>
2025-11-09 21:53:44 -0800

    ahora si

--- src/app_streamlit.py
+++ src/app_streamlit.py
@@ -5,16 +5,17 @@ import numpy as np
 model = joblib.load("models/best_diabetes_model.joblib")
 
 st.title("Predicción de Diabetes")
+st.write("Ingrese los datos del paciente para predecir si tiene diabetes.")
 
 # Inputs
-pregnancies = st.number_input("Pregnancies", min_value=0, max_value=20, value=1)
-glucose = st.number_input("Glucose", min_value=0.0, max_value=300.0, value=125.0)
-bloodpressure = st.number_input("BloodPressure", min_value=0.0, max_value=140.0, value=70.0)
-skinthickness = st.number_input("SkinThickness", min_value=0.0, max_value=100.0, value=20.0)
-insulin = st.number_input("Insulin", min_value=0.0, max_value=900.0, value=80.0)
-bmi = st.number_input("BMI", min_value=0.0, max_value=80.0, value=28.0)
-diabetes_pedigree = st.number_input("DiabetesPedigreeFunction", min_value=0.0, max_value=5.0, value=0.5)
-age = st.number_input("Age", min_value=0, max_value=120, value=33)
+pregnancies = st.number_input("Embarazos", min_value=0, max_value=20, value=1)
+glucose = st.number_input("Nivel de Glucosa", min_value=0.0, max_value=300.0, value=125.0)
+bloodpressure = st.number_input("Presión Arterial", min_value=0.0, max_value=140.0, value=70.0)
+skinthickness = st.number_input("Grosor de la piel", min_value=0.0, max_value=100.0, value=20.0)
+insulin = st.number_input("Insulina", min_value=0.0, max_value=900.0, value=80.0)
+bmi = st.number_input("IMC", min_value=0.0, max_value=80.0, value=28.0)
+diabetes_pedigree = st.number_input("DiabetesPedigreeFunction (porcentaje de herencia familiar)", min_value=0.0, max_value=5.0, value=0.5)
+age = st.number_input("Edad", min_value=0, max_value=120, value=33)
 
 if st.button("Predecir"):
     X_new = np.array([[pregnancies, glucose, bloodpressure, skinthickness, insulin, bmi, diabetes_pedigree, age]])
